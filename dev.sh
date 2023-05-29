d57689f041eefb0de4bdaa2f3c4ea93856c1ae20
Yilmaz Mustafa <dev@mail.be>
2023-05-29 23:23:09 +0200

    init..

---
+++ dev.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+pip install \
+    requests \
+    setuptools \
+    build \
+    black \
+    coverage \
+    flake8 \
+    pep8-naming \
+    pytest \
+    pytest-cov \
+    pytest-mock \
+    pytest-profiling \
+    wheel \
+    twine \
+    pylint \
+    autopep8 \
+    pyinstaller \
+    opencv-python \
+    mediapipe \
+    cvzone \
+    pandas \
+    python-dotenv \
+    kivy[dev] \
+    keras \
+    jupyter \
+    jupyterlab \
+    jupyterlab-code-formatter \
+    jupyterlab-git \
+    jupyterlab-lsp \
+    pynput
+
+
+
